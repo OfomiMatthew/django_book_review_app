23aba6821ebea1c4609aab28c8ae68cfbafbdd20
MatthewOfomi <ofomimatthew7@gmail.com>
2024-03-02 12:00:13 +0100

    query parameters techniques

--- src/Reviews/views.py
+++ src/Reviews/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  print(request.headers)
-  return HttpResponse('<h1>Welcome to BookRev</h1>')
+  name = request.GET.get('name') or "world"
+  last = request.GET.get("last_name")
+  print(name)
+  return render(request,'base.html',{'name':name,'last':last})
